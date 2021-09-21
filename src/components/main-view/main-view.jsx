b29e34a6dfd1fe61a9009ba8e7baeb6e37a2e9ef
jbmitchell7 <jbmitchell747@gmail.com>
2021-09-21 13:20:34 -0500

    moved image folder

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,9 +8,9 @@ export class MainView extends React.Component {
         super();
         this.state = {
             movies: [
-                { _id: 1, Title: 'Jurassic Park', Description: 'A wealthy entrepreneur secretly creates a theme park featuring living dinosaurs drawn from prehistoric DNA. Before opening day, he invites a team of experts and his two eager grandchildren to experience the park and help calm anxious investors.', ImagePath: '../../../img/jurassicpark.jpg' },
-                { _id: 2, Title: 'Blade Runner', Description: 'In the smog-choked dystopian Los Angeles of 2019, blade runner Rick Deckard is called out of retirement to terminate a quartet of replicants who have escaped to Earth seeking their creator for a way to extend their short life spans.', ImagePath: '../../../img/bladerunner.jpg' },
-                { _id: 3, Title: 'Jaws', Description: 'When an insatiable great white shark terrorizes the townspeople of Amity Island, the police chief, an oceanographer and a grizzled shark hunter seek to destroy the blood-thirsty beast.', ImagePath: '../../../img/jaws.jpg' }
+                { _id: 1, Title: 'Jurassic Park', Description: 'A wealthy entrepreneur secretly creates a theme park featuring living dinosaurs drawn from prehistoric DNA. Before opening day, he invites a team of experts and his two eager grandchildren to experience the park and help calm anxious investors.', ImagePath: '../../img/jurassicpark.jpg' },
+                { _id: 2, Title: 'Blade Runner', Description: 'In the smog-choked dystopian Los Angeles of 2019, blade runner Rick Deckard is called out of retirement to terminate a quartet of replicants who have escaped to Earth seeking their creator for a way to extend their short life spans.', ImagePath: '../../img/bladerunner.jpg' },
+                { _id: 3, Title: 'Jaws', Description: 'When an insatiable great white shark terrorizes the townspeople of Amity Island, the police chief, an oceanographer and a grizzled shark hunter seek to destroy the blood-thirsty beast.', ImagePath: '../../img/jaws.jpg' }
             ],
             selectedMovie: null
         };
