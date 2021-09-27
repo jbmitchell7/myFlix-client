556919083932e34708f5b581ac19b2f61c17d555
jbmitchell7 <jbmitchell747@gmail.com>
2021-09-27 11:34:00 -0500

    added styling files and made some changes for the exercise

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -7,13 +9,21 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Jurassic Park', Description: 'A wealthy entrepreneur secretly creates a theme park featuring living dinosaurs drawn from prehistoric DNA. Before opening day, he invites a team of experts and his two eager grandchildren to experience the park and help calm anxious investors.', ImagePath: '../../img/jurassicpark.jpg' },
-                { _id: 2, Title: 'Blade Runner', Description: 'In the smog-choked dystopian Los Angeles of 2019, blade runner Rick Deckard is called out of retirement to terminate a quartet of replicants who have escaped to Earth seeking their creator for a way to extend their short life spans.', ImagePath: '../../img/bladerunner.jpg' },
-                { _id: 3, Title: 'Jaws', Description: 'When an insatiable great white shark terrorizes the townspeople of Amity Island, the police chief, an oceanographer and a grizzled shark hunter seek to destroy the blood-thirsty beast.', ImagePath: '../../img/jaws.jpg' }
-            ],
+            movies: [],
             selectedMovie: null
         };
+    }
+
+    componentDidMount() {
+        axios.get('https://jakesmoviedb.herokuapp.com/movies')
+            .then((response) => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -25,14 +35,14 @@ export class MainView extends React.Component {
     render() {
         const { movies, selectedMovie } = this.state;
 
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        if (movies.length === 0) return <div className="main-view" />;
 
         return (
             <div className="main-view">
                 {selectedMovie
                     ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
                     : movies.map(movie => (
-                        <MovieCard key={movie._id} movieData={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+                        <MovieCard key={movie._id} movieData={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
                     ))}
             </div>
         );
